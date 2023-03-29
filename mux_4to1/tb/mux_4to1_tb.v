ef4f4c86053c83210dddcb89a3f88e62a5eda09d
Akshay Dandekar <47881557+AkshayD13@users.noreply.github.com>
2023-03-29 07:15:27 +0530

    Update mux_4to1_tb.v

--- mux_4to1/tb/mux_4to1_tb.v
+++ mux_4to1/tb/mux_4to1_tb.v
@@ -2,36 +2,60 @@ module mux_4to1_tb;
 reg [3:0]d;
 reg [1:0]select;
 wire y_test;
+reg test_complete, error_flag;
 mux_4to1_case dut1(.d(d),.select(select),.y(y_test));
 initial
 begin
 	repeat(100)
 	begin
-		d=$random % 16;
+		#10 d=$random % 16;
 		select= $random % 16;
-		#10
-		$display($time,"d=%b,select=%b,y=%b",d,select,y_test);
-		if(select==2'b00)
+	end
+	test_complete=1;
+end
+always @(y_test)
+begin
+	error_flag=0;
+	#5 $display($time,"d=%b,select=%b,y_test=%b",d,select,y_test);
+	if(select==2'b00)
+	begin
+		if(y_test!=d[0])
 		begin
-			if(y_test!=d[0])
-				$display("Error!! expected: y=%b, actual: %b",y_test,d[0]);
+			$display("Error!! expected: y=%b, actual: y_test= %b",d[0], y_test);
+			error_flag=1;
 		end
-		if(select==2'b01)
+	end
+	if(select==2'b01)
+	begin
+		if(y_test!=d[1])
 		begin
-			if(y_test!=d[1])
-				$display("Error!! expected: y=%b, actual: %b",y_test,d[1]);
+			$display("Error!! expected: y=%b, actual: y_test= %b",d[1], y_test);
+			error_flag=1;
 		end
-		if(select==2'b10)
+	end
+	if(select==2'b10)
+	begin
+		if(y_test!=d[2])
 		begin
-			if(y_test!=d[2])
-				$display("Error!! expected: y=%b, actual: %b",y_test,d[2]);
+			$display("Error!! expected: y=%b, actual: y_test= %b",d[2], y_test);
+			error_flag=1;
 		end
-		if(select==2'b11)
+	end
+	if(select==2'b11)
+	begin
+		if(y_test!=d[3])
 		begin
-			if(y_test!=d[3])
-				$display("Error!! expected: y=%b, actual: %b",y_test,d[3]);
+			$display("Error!! expected: y=%b, actual: y_test= %b",d[3], y_test);
+			error_flag=1;
 		end
 	end
 end
+always @(test_complete)
+begin
+	if(error_flag)
+		$display("Tests failed");
+	else
+		$display("Tests passed");
+end 
 endmodule
 	
