3a8ed1f42e7fa5700d63c4fcaf892880fd9fa788
Akshay Dandekar <47881557+AkshayD13@users.noreply.github.com>
2023-03-30 07:20:47 +0530

    Update fsm_111010_case.v

--- fsm_111010/rtl/fsm_111010_case.v
+++ fsm_111010/rtl/fsm_111010_case.v
@@ -19,62 +19,62 @@ begin
 end
 always @(p_state,x)
 begin
-	y<=0;
+	y=0;
 	case(p_state)
 	s0:
 	if(x==0)
 	begin
-		n_state<=s0;
+		n_state=s0;
 	end
 	else
 	begin
-		n_state<=s1;
+		n_state=s1;
 	end
 	s1:
 	if(x==0)
 	begin
-		n_state<=s0;
+		n_state=s0;
 	end
 	else
 	begin
-		n_state<=s2;
+		n_state=s2;
 	end
 	s2:
 	if(x==0)
 	begin
-		n_state<=s0;
+		n_state=s0;
 	end
 	else
 	begin
-		n_state<=s3;
+		n_state=s3;
 	end
 	s3:
 	if(x==0)
 	begin
-		n_state<=s4;
+		n_state=s4;
 	end
 	else
 	begin
-		n_state<=s3;
+		n_state=s3;
 	end
 	s4:
 	if(x==0)
 	begin
-		n_state<=s0;
+		n_state=s0;
 	end
 	else
 	begin
-		n_state<=s5;
+		n_state=s5;
 	end
 	s5:
 	if(x==0)
 	begin
-		n_state<=s0;
+		n_state=s0;
 		y<=1;
 	end
 	else
 	begin
-		n_state<=s2;
+		n_state=s2;
 	end
 	endcase
 end
