6ff74f5a88f464a75b95fb7668de1fa41286a3ac
Akshay Dandekar <47881557+AkshayD13@users.noreply.github.com>
2023-04-16 22:34:50 +0530

    Update fifo_synchronous.v

--- fifo_synchronous/rtl/fifo_synchronous.v
+++ fifo_synchronous/rtl/fifo_synchronous.v
@@ -2,7 +2,7 @@ module FIFO_synchronous(input [7:0]data_in, input clk,rst,rd,wr,output reg empty
 reg [7:0]FIFO_memory[7:0];				//8 memories each of size 8-bits declared
 	reg [2:0]rd_ptr,wr_ptr;				//3 bit pointer declared as 8 memory locations are present. 4-bit FIFO_count is taken in order to flag full, as it's not possible if 3 bit FIFO_count can count till only 7
 always @(posedge clk)
-begin:write						//write operation while block
+begin							//write operation while block
 	if(wr && !full)
 	begin
 		FIFO_memory[wr_ptr]<=data_in;
@@ -14,7 +14,7 @@ begin:write						//write operation while block
 end
 
 always @(posedge clk)
-begin:read						//read operation while block
+begin							//read operation while block
 	if(rd && !empty)
 	begin
 		data_out<=FIFO_memory[rd_ptr];
@@ -26,7 +26,7 @@ begin:read						//read operation while block
 end
 
 always @(posedge clk)
-begin:pointer
+begin
 	if(rst)
 	begin
 		{wr_ptr,rd_ptr}<=0;
@@ -39,7 +39,7 @@ begin:pointer
 end
 
 always @(posedge clk)
-begin:count
+begin
 	if(rst)
 	begin
 		FIFO_count<=0;
